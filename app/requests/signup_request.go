85f2291d476cdfcc6025e8d507b6717274e48a9a
Summer <summer@learnku.com>
2022-01-01 22:54:56 +0800

    Email 是否已注册接口

--- app/requests/signup_request.go
+++ app/requests/signup_request.go
@@ -36,3 +36,36 @@ func ValidateSignupPhoneExist(data interface{}, c *gin.Context) map[string][]str
 	// 开始验证
 	return govalidator.New(opts).ValidateStruct()
 }
+
+type SignupEmailExistRequest struct {
+	Email string `json:"email,omitempty" valid:"email"`
+}
+
+func ValidateSignupEmailExist(data interface{}, c *gin.Context) map[string][]string {
+
+	// 自定义验证规则
+	rules := govalidator.MapData{
+		"email": []string{"required", "min:4", "max:30", "email"},
+	}
+
+	// 自定义验证出错时的提示
+	messages := govalidator.MapData{
+		"email": []string{
+			"required:Email 为必填项",
+			"min:Email 长度需大于 4",
+			"max:Email 长度需小于 30",
+			"email:Email 格式不正确，请提供有效的邮箱地址",
+		},
+	}
+
+	// 配置初始化
+	opts := govalidator.Options{
+		Data:          data,
+		Rules:         rules,
+		TagIdentifier: "valid", // 模型中的 Struct 标签标识符
+		Messages:      messages,
+	}
+
+	// 开始验证
+	return govalidator.New(opts).ValidateStruct()
+}
--- routes/api.go
+++ routes/api.go
@@ -18,6 +18,8 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			suc := new(auth.SignupController)
 			// 判断手机是否已注册
 			authGroup.POST("/signup/phone/exist", suc.IsPhoneExist)
+			// 判断 Email 是否已注册
+			authGroup.POST("/signup/email/exist", suc.IsEmailExist)
 		}
 	}
 }
