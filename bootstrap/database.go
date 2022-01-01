8e05876c14ad708e6feb0fdfd78ef0c3a2df6fdd
Summer <summer@learnku.com>
2022-01-01 21:54:20 +0800

    手机是否注册接口

--- bootstrap/database.go
+++ bootstrap/database.go
@@ -3,6 +3,7 @@ package bootstrap
 import (
 	"errors"
 	"fmt"
+	"gohub/app/models/user"
 	"gohub/pkg/config"
 	"gohub/pkg/database"
 	"time"
@@ -49,4 +50,5 @@ func SetupDB() {
 	// 设置每个链接的过期时间
 	database.SQLDB.SetConnMaxLifetime(time.Duration(config.GetInt("database.mysql.max_life_seconds")) * time.Second)
 
+	database.DB.AutoMigrate(&user.User{})
 }
