427f020a60879232f174b02d56414f7322f7df28
Summer <summer@learnku.com>
2022-01-01 17:27:56 +0800

    用户模型

---
+++ app/models/model.go
@@ -0,0 +1,17 @@
+// Package models 模型通用属性和方法
+package models
+
+import (
+	"time"
+)
+
+// CommonIDField 模型基类
+type CommonIDField struct {
+	ID uint64 `gorm:"column:id;primaryKey;autoIncrement;" json:"id,omitempty"`
+}
+
+// CommonTimestampsField 时间戳
+type CommonTimestampsField struct {
+	CreatedAt time.Time `gorm:"column:created_at;index;" json:"created_at,omitempty"`
+	UpdatedAt time.Time `gorm:"column:updated_at;index;" json:"updated_at,omitempty"`
+}
