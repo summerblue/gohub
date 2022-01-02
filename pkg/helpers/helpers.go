445a7cefae4a0ad7e9feb138a764b452410f27a9
Summer <summer@learnku.com>
2022-01-02 21:22:45 +0800

    HTTP 访问日志

---
+++ pkg/helpers/helpers.go
@@ -0,0 +1,13 @@
+// Package helpers 存放辅助方法
+package helpers
+
+import (
+	"fmt"
+	"time"
+)
+
+// MicrosecondsStr 将 time.Duration 类型（nano seconds 为单位）
+// 输出为小数点后 3 位的 ms （microsecond 毫秒，千分之一秒）
+func MicrosecondsStr(elapsed time.Duration) string {
+	return fmt.Sprintf("%.3fms", float64(elapsed.Nanoseconds())/1e6)
+}
