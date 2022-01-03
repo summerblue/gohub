ecf3db5b5b552f0b3f21a9ed530edfae12bcab66
Summer <summer@learnku.com>
2022-01-03 22:49:09 +0800

    发送邮件（mail 包）

---
+++ pkg/mail/mail.go
@@ -0,0 +1,44 @@
+// Package mail 发送短信
+package mail
+
+import (
+	"gohub/pkg/config"
+	"sync"
+)
+
+type From struct {
+	Address string
+	Name    string
+}
+
+type Email struct {
+	From    From
+	To      []string
+	Bcc     []string
+	Cc      []string
+	Subject string
+	Text    []byte // Plaintext message (optional)
+	HTML    []byte // Html message (optional)
+}
+
+type Mailer struct {
+	Driver Driver
+}
+
+var once sync.Once
+var internalMailer *Mailer
+
+// NewMailer 单例模式获取
+func NewMailer() *Mailer {
+	once.Do(func() {
+		internalMailer = &Mailer{
+			Driver: &SMTP{},
+		}
+	})
+
+	return internalMailer
+}
+
+func (mailer *Mailer) Send(email Email) bool {
+	return mailer.Driver.Send(email, config.GetStringMapString("mail.smtp"))
+}
