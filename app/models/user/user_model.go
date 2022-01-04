692a46ee6db6a72589b6fd47601eb655f5bdcd27
Summer <summer@learnku.com>
2022-01-04 20:33:49 +0800

    通过手机找回密码

--- app/models/user/user_model.go
+++ app/models/user/user_model.go
@@ -28,3 +28,8 @@ func (userModel *User) Create() {
 func (userModel *User) ComparePassword(_password string) bool {
 	return hash.BcryptCheck(_password, userModel.Password)
 }
+
+func (userModel *User) Save() (rowsAffected int64) {
+	result := database.DB.Save(&userModel)
+	return result.RowsAffected
+}
