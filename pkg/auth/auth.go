84b29e56048bedc6272b34e61725f95c3b8bb251
Summer <summer@learnku.com>
2022-01-04 19:14:56 +0800

    Auth 中间件

--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -4,6 +4,9 @@ package auth
 import (
 	"errors"
 	"gohub/app/models/user"
+	"gohub/pkg/logger"
+
+	"github.com/gin-gonic/gin"
 )
 
 // Attempt 尝试登录
@@ -28,4 +31,20 @@ func LoginByPhone(phone string) (user.User, error) {
 	}
 
 	return userModel, nil
+}
+
+// CurrentUser 从 gin.context 中获取当前登录用户
+func CurrentUser(c *gin.Context) user.User {
+	userModel, ok := c.MustGet("current_user").(user.User)
+	if !ok {
+		logger.LogIf(errors.New("无法获取用户"))
+		return user.User{}
+	}
+	// db is now a *DB value
+	return userModel
+}
+
+// CurrentUID 从 gin.context 中获取当前登录用户 ID
+func CurrentUID(c *gin.Context) string {
+	return c.GetString("current_user_id")
 }
