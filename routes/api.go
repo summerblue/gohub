4c546fa10b06ef402e59da906777935b08e4e3cf
Summer <summer@learnku.com>
2022-01-06 16:19:03 +0800

    删除话题

--- routes/api.go
+++ routes/api.go
@@ -75,6 +75,7 @@ func RegisterAPIRoutes(r *gin.Engine) {
 		{
 			tpcGroup.POST("", middlewares.AuthJWT(), tpc.Store)
 			tpcGroup.PUT("/:id", middlewares.AuthJWT(), tpc.Update)
+			tpcGroup.DELETE("/:id", middlewares.AuthJWT(), tpc.Delete)
 		}
 	}
 }
