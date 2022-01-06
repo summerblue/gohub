99465c280412395cbe8f7602e5a7f90e8e8e97e2
Summer <summer@learnku.com>
2022-01-06 09:37:42 +0800

    验证分页请求

--- app/http/controllers/api/v1/users_controller.go
+++ app/http/controllers/api/v1/users_controller.go
@@ -2,6 +2,7 @@ package v1
 
 import (
 	"gohub/app/models/user"
+	"gohub/app/requests"
 	"gohub/pkg/auth"
 	"gohub/pkg/response"
 
@@ -20,6 +21,11 @@ func (ctrl *UsersController) CurrentUser(c *gin.Context) {
 
 // Index 所有用户
 func (ctrl *UsersController) Index(c *gin.Context) {
+	request := requests.PaginationRequest{}
+	if ok := requests.Validate(c, &request, requests.Pagination); !ok {
+		return
+	}
+
 	data, pager := user.Paginate(c, 10)
 	response.JSON(c, gin.H{
 		"data":  data,
