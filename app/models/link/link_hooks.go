20b947da5262337fbe6bcde1bda2952b82a8cf9f
Summer <summer@learnku.com>
2022-01-06 16:59:47 +0800

    友情链接模型和迁移

---
+++ app/models/link/link_hooks.go
@@ -0,0 +1,11 @@
+package link
+
+// func (link *Link) BeforeSave(tx *gorm.DB) (err error) {}
+// func (link *Link) BeforeCreate(tx *gorm.DB) (err error) {}
+// func (link *Link) AfterCreate(tx *gorm.DB) (err error) {}
+// func (link *Link) BeforeUpdate(tx *gorm.DB) (err error) {}
+// func (link *Link) AfterUpdate(tx *gorm.DB) (err error) {}
+// func (link *Link) AfterSave(tx *gorm.DB) (err error) {}
+// func (link *Link) BeforeDelete(tx *gorm.DB) (err error) {}
+// func (link *Link) AfterDelete(tx *gorm.DB) (err error) {}
+// func (link *Link) AfterFind(tx *gorm.DB) (err error) {}
