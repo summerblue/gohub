8eed7d420f9b5ce1a9613e8df044bf405791200c
Summer <summer@learnku.com>
2022-01-06 10:32:38 +0800

    分类模型

---
+++ app/models/category/category_hooks.go
@@ -0,0 +1,11 @@
+package category
+
+// func (category *Category) BeforeSave(tx *gorm.DB) (err error) {}
+// func (category *Category) BeforeCreate(tx *gorm.DB) (err error) {}
+// func (category *Category) AfterCreate(tx *gorm.DB) (err error) {}
+// func (category *Category) BeforeUpdate(tx *gorm.DB) (err error) {}
+// func (category *Category) AfterUpdate(tx *gorm.DB) (err error) {}
+// func (category *Category) AfterSave(tx *gorm.DB) (err error) {}
+// func (category *Category) BeforeDelete(tx *gorm.DB) (err error) {}
+// func (category *Category) AfterDelete(tx *gorm.DB) (err error) {}
+// func (category *Category) AfterFind(tx *gorm.DB) (err error) {}
