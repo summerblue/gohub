35f418316931ac5cac79229c9c1790fd823d11b3
Summer <summer@learnku.com>
2022-01-06 15:58:13 +0800

    话题更新

--- routes/api.go
+++ routes/api.go
@@ -74,6 +74,7 @@ func RegisterAPIRoutes(r *gin.Engine) {
 		tpcGroup := v1.Group("/topics")
 		{
 			tpcGroup.POST("", middlewares.AuthJWT(), tpc.Store)
+			tpcGroup.PUT("/:id", middlewares.AuthJWT(), tpc.Update)
 		}
 	}
 }
