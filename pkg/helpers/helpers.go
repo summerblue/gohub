da844955333dd42c1ff9c900fd232a8fee22cb44
Summer <summer@learnku.com>
2022-01-08 17:44:38 +0800

    HTTP 访问日志

--- pkg/helpers/helpers.go
+++ pkg/helpers/helpers.go
@@ -1,7 +1,11 @@
 // Package helpers 存放辅助方法
 package helpers
 
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+	"time"
+)
 
 // Empty 类似于 PHP 的 empty() 函数
 func Empty(val interface{}) bool {
@@ -26,4 +30,10 @@ func Empty(val interface{}) bool {
 		return v.IsNil()
 	}
 	return reflect.DeepEqual(val, reflect.Zero(v.Type()).Interface())
+}
+
+// MicrosecondsStr 将 time.Duration 类型（nano seconds 为单位）
+// 输出为小数点后 3 位的 ms （microsecond 毫秒，千分之一秒）
+func MicrosecondsStr(elapsed time.Duration) string {
+	return fmt.Sprintf("%.3fms", float64(elapsed.Nanoseconds())/1e6)
 }
