b52520458569995037fcd4f5c298828fbaa5b176
Summer <summer@learnku.com>
2022-01-08 21:05:50 +0800

    通过邮箱找回密码

--- app/http/controllers/api/v1/auth/password_controller.go
+++ app/http/controllers/api/v1/auth/password_controller.go
@@ -34,3 +34,22 @@ func (pc *PasswordController) ResetByPhone(c *gin.Context) {
 		response.Success(c)
 	}
 }
+
+// ResetByEmail 使用 Email 和验证码重置密码
+func (pc *PasswordController) ResetByEmail(c *gin.Context) {
+	// 1. 验证表单
+	request := requests.ResetByEmailRequest{}
+	if ok := requests.Validate(c, &request, requests.ResetByEmail); !ok {
+		return
+	}
+
+	// 2. 更新密码
+	userModel := user.GetByEmail(request.Email)
+	if userModel.ID == 0 {
+		response.Abort404(c)
+	} else {
+		userModel.Password = request.Password
+		userModel.Save()
+		response.Success(c)
+	}
+}
