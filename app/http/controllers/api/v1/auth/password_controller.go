cc9d30ca5fddfe06eba347e3b6849ab19cf5b59b
Summer <summer@learnku.com>
2022-01-08 21:04:57 +0800

    通过手机找回密码

---
+++ app/http/controllers/api/v1/auth/password_controller.go
@@ -0,0 +1,36 @@
+// Package auth 处理用户注册、登录、密码重置
+package auth
+
+import (
+	v1 "gohub/app/http/controllers/api/v1"
+	"gohub/app/models/user"
+	"gohub/app/requests"
+	"gohub/pkg/response"
+
+	"github.com/gin-gonic/gin"
+)
+
+// PasswordController 用户控制器
+type PasswordController struct {
+	v1.BaseAPIController
+}
+
+// ResetByPhone 使用手机和验证码重置密码
+func (pc *PasswordController) ResetByPhone(c *gin.Context) {
+	// 1. 验证表单
+	request := requests.ResetByPhoneRequest{}
+	if ok := requests.Validate(c, &request, requests.ResetByPhone); !ok {
+		return
+	}
+
+	// 2. 更新密码
+	userModel := user.GetByPhone(request.Phone)
+	if userModel.ID == 0 {
+		response.Abort404(c)
+	} else {
+		userModel.Password = request.Password
+		userModel.Save()
+
+		response.Success(c)
+	}
+}
