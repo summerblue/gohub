e4b0b9e3233bd99b24f83ec235e392fd10268210
Summer <summer@learnku.com>
2022-01-08 17:40:51 +0800

    初始化日志库

--- main.go
+++ main.go
@@ -23,6 +23,9 @@ func main() {
 	flag.Parse()
 	config.InitConfig(env)
 
+	// 初始化 Logger
+	bootstrap.SetupLogger()
+
 	// new 一个 Gin Engine 实例
 	router := gin.New()
 
---
+++ pkg/app/app.go
@@ -0,0 +1,18 @@
+// Package app 应用信息
+package app
+
+import (
+	"gohub/pkg/config"
+)
+
+func IsLocal() bool {
+	return config.Get("app.env") == "local"
+}
+
+func IsProduction() bool {
+	return config.Get("app.env") == "production"
+}
+
+func IsTesting() bool {
+	return config.Get("app.env") == "testing"
+}
