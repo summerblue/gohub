d3e42bc23059465c40fd647831ab77a2cd6cbf07
Summer <summer@learnku.com>
2022-01-08 17:01:50 +0800

    配置方案的实现

---
+++ config/config.go
@@ -0,0 +1,6 @@
+// Package config 存放程序所有的配置信息
+package config
+
+// Initialize 触发加载 config 包的所有 init 函数
+func Initialize() {
+}
--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"gohub/bootstrap"
+	btsConfig "gohub/config"
+	"gohub/pkg/config"
 
 	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	// 加载 config 目录下的配置信息
+	btsConfig.Initialize()
+}
+
 func main() {
+
+	// 配置初始化，依赖命令行 --env 参数
+	var env string
+	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
+	flag.Parse()
+	config.InitConfig(env)
 
 	// new 一个 Gin Engine 实例
 	router := gin.New()
@@ -16,7 +30,7 @@ func main() {
 	bootstrap.SetupRoute(router)
 
 	// 运行服务
-	err := router.Run(":3000")
+	err := router.Run(":" + config.Get("app.port"))
 	if err != nil {
 		// 错误处理，端口被占用了或者其他错误
 		fmt.Println(err.Error())
