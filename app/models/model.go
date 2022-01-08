99667f0374a343335a09d7305ce89cbe294388e3
Summer <summer@learnku.com>
2022-01-08 19:45:31 +0800

    JWT 授权

--- app/models/model.go
+++ app/models/model.go
@@ -3,6 +3,8 @@ package models
 
 import (
 	"time"
+
+	"github.com/spf13/cast"
 )
 
 // BaseModel 模型基类
@@ -14,4 +16,9 @@ type BaseModel struct {
 type CommonTimestampsField struct {
 	CreatedAt time.Time `gorm:"column:created_at;index;" json:"created_at,omitempty"`
 	UpdatedAt time.Time `gorm:"column:updated_at;index;" json:"updated_at,omitempty"`
+}
+
+// GetStringID 获取 ID 的字符串格式
+func (a BaseModel) GetStringID() string {
+	return cast.ToString(a.ID)
 }
