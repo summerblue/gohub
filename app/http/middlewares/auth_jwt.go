34a16d12d7f08a8ba1f81a06c79e23d61bcab70e
Summer <summer@learnku.com>
2022-01-08 20:33:29 +0800

    Auth 中间件

---
+++ app/http/middlewares/auth_jwt.go
@@ -0,0 +1,40 @@
+// Package middlewares Gin 中间件
+package middlewares
+
+import (
+	"fmt"
+	"gohub/app/models/user"
+	"gohub/pkg/config"
+	"gohub/pkg/jwt"
+	"gohub/pkg/response"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthJWT() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		// 从标头 Authorization:Bearer xxxxx 中获取信息，并验证 JWT 的准确性
+		claims, err := jwt.NewJWT().ParserToken(c)
+
+		// JWT 解析失败，有错误发生
+		if err != nil {
+			response.Unauthorized(c, fmt.Sprintf("请查看 %v 相关的接口认证文档", config.GetString("app.name")))
+			return
+		}
+
+		// JWT 解析成功，设置用户信息
+		userModel := user.Get(claims.UserID)
+		if userModel.ID == 0 {
+			response.Unauthorized(c, "找不到对应用户，用户可能已删除")
+			return
+		}
+
+		// 将用户信息存入 gin.context 里，后续 auth 包将从这里拿到当前用户数据
+		c.Set("current_user_id", userModel.GetStringID())
+		c.Set("current_user_name", userModel.Name)
+		c.Set("current_user", userModel)
+
+		c.Next()
+	}
+}
