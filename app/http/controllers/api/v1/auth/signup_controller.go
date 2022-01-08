00106750af100432be2f85fc6368d12c7fa5a49e
Summer <summer@learnku.com>
2022-01-08 17:18:08 +0800

    手机是否注册接口

---
+++ app/http/controllers/api/v1/auth/signup_controller.go
@@ -0,0 +1,43 @@
+// Package auth 处理用户身份认证相关逻辑
+package auth
+
+import (
+	"fmt"
+	v1 "gohub/app/http/controllers/api/v1"
+	"gohub/app/models/user"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// SignupController 注册控制器
+type SignupController struct {
+	v1.BaseAPIController
+}
+
+// IsPhoneExist 检测手机号是否被注册
+func (sc *SignupController) IsPhoneExist(c *gin.Context) {
+
+	// 请求对象
+	type PhoneExistRequest struct {
+		Phone string `json:"phone"`
+	}
+	request := PhoneExistRequest{}
+
+	// 解析 JSON 请求
+	if err := c.ShouldBindJSON(&request); err != nil {
+		// 解析失败，返回 422 状态码和错误信息
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"error": err.Error(),
+		})
+		// 打印错误信息
+		fmt.Println(err.Error())
+		// 出错了，中断请求
+		return
+	}
+
+	//  检查数据库并返回响应
+	c.JSON(http.StatusOK, gin.H{
+		"exist": user.IsPhoneExist(request.Phone),
+	})
+}
---
+++ app/http/controllers/api/v1/base_api_controller.go
@@ -0,0 +1,6 @@
+// Package v1 处理业务逻辑, GoHub 控制器 v1
+package v1
+
+// BaseAPIController 基础控制器
+type BaseAPIController struct {
+}
