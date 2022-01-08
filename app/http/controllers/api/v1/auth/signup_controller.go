177b049f951e485761af76337bcc0bab302e3524
Summer <summer@learnku.com>
2022-01-08 17:21:16 +0800

    Email 是否已注册接口

--- app/http/controllers/api/v1/auth/signup_controller.go
+++ app/http/controllers/api/v1/auth/signup_controller.go
@@ -50,3 +50,38 @@ func (sc *SignupController) IsPhoneExist(c *gin.Context) {
 		"exist": user.IsPhoneExist(request.Phone),
 	})
 }
+
+// IsEmailExist 检测邮箱是否已注册
+func (sc *SignupController) IsEmailExist(c *gin.Context) {
+
+	// 初始化请求对象
+	request := requests.SignupEmailExistRequest{}
+
+	// 解析 JSON 请求
+	if err := c.ShouldBindJSON(&request); err != nil {
+		// 解析失败，返回 422 状态码和错误信息
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"error": err.Error(),
+		})
+		// 打印错误信息
+		fmt.Println(err.Error())
+		// 出错了，中断请求
+		return
+	}
+
+	// 表单验证
+	errs := requests.ValidateSignupEmailExist(&request, c)
+	// errs 返回长度等于零即通过，大于 0 即有错误发生
+	if len(errs) > 0 {
+		// 验证失败，返回 422 状态码和错误信息
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"errors": errs,
+		})
+		return
+	}
+
+	//  检查数据库并返回响应
+	c.JSON(http.StatusOK, gin.H{
+		"exist": user.IsEmailExist(request.Email),
+	})
+}
