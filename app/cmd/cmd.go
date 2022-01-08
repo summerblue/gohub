95ddd6854fd3deacde1add27c127f94bfe1aee34
Summer <summer@learnku.com>
2022-01-08 21:16:25 +0800

    命令行模式

---
+++ app/cmd/cmd.go
@@ -0,0 +1,27 @@
+// Package cmd 存放程序的所有子命令
+package cmd
+
+import (
+	"gohub/pkg/helpers"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// Env 存储全局选项 --env 的值
+var Env string
+
+// RegisterGlobalFlags 注册全局选项（flag）
+func RegisterGlobalFlags(rootCmd *cobra.Command) {
+	rootCmd.PersistentFlags().StringVarP(&Env, "env", "e", "", "load .env file, example: --env=testing will use .env.testing file")
+}
+
+// RegisterDefaultCmd 注册默认命令
+func RegisterDefaultCmd(rootCmd *cobra.Command, subCmd *cobra.Command) {
+	cmd, _, err := rootCmd.Find(os.Args[1:])
+	firstArg := helpers.FirstElement(os.Args[1:])
+	if err == nil && cmd.Use == rootCmd.Use && firstArg != "-h" && firstArg != "--help" {
+		args := append([]string{subCmd.Use}, os.Args[1:]...)
+		rootCmd.SetArgs(args)
+	}
+}
---
+++ app/cmd/serve.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"gohub/bootstrap"
+	"gohub/pkg/config"
+	"gohub/pkg/console"
+	"gohub/pkg/logger"
+
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/cobra"
+)
+
+// CmdServe represents the available web sub-command.
+var CmdServe = &cobra.Command{
+	Use:   "serve",
+	Short: "Start web server",
+	Run:   runWeb,
+	Args:  cobra.NoArgs,
+}
+
+func runWeb(cmd *cobra.Command, args []string) {
+
+	// 设置 gin 的运行模式，支持 debug, release, test
+	// release 会屏蔽调试信息，官方建议生产环境中使用
+	// 非 release 模式 gin 终端打印太多信息，干扰到我们程序中的 Log
+	// 故此设置为 release，有特殊情况手动改为 debug 即可
+	gin.SetMode(gin.ReleaseMode)
+
+	// gin 实例
+	router := gin.New()
+
+	// 初始化路由绑定
+	bootstrap.SetupRoute(router)
+
+	// 运行服务器
+	err := router.Run(":" + config.Get("app.port"))
+	if err != nil {
+		logger.ErrorString("CMD", "serve", err.Error())
+		console.Exit("Unable to start server, error:" + err.Error())
+	}
+}
