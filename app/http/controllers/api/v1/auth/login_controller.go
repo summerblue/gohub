4bb9111daa063ed3382a03c4e4db88e4302076f9
Summer <summer@learnku.com>
2022-01-08 20:32:36 +0800

    Refresh Token

--- app/http/controllers/api/v1/auth/login_controller.go
+++ app/http/controllers/api/v1/auth/login_controller.go
@@ -60,3 +60,17 @@ func (lc *LoginController) LoginByPassword(c *gin.Context) {
 		})
 	}
 }
+
+// RefreshToken 刷新 Access Token
+func (lc *LoginController) RefreshToken(c *gin.Context) {
+
+	token, err := jwt.NewJWT().RefreshToken(c)
+
+	if err != nil {
+		response.Error(c, err, "令牌刷新失败")
+	} else {
+		response.JSON(c, gin.H{
+			"token": token,
+		})
+	}
+}
