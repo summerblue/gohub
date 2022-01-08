142e478729dfe2ed4a2ebe210cc4ebf69ff890c0
Summer <summer@learnku.com>
2022-01-08 18:52:15 +0800

    手机号+短信验证码注册

--- app/http/controllers/api/v1/auth/signup_controller.go
+++ app/http/controllers/api/v1/auth/signup_controller.go
@@ -39,3 +39,29 @@ func (sc *SignupController) IsEmailExist(c *gin.Context) {
 		"exist": user.IsEmailExist(request.Email),
 	})
 }
+
+// SignupUsingPhone 使用手机和验证码进行注册
+func (sc *SignupController) SignupUsingPhone(c *gin.Context) {
+
+	// 1. 验证表单
+	request := requests.SignupUsingPhoneRequest{}
+	if ok := requests.Validate(c, &request, requests.SignupUsingPhone); !ok {
+		return
+	}
+
+	// 2. 验证成功，创建数据
+	_user := user.User{
+		Name:     request.Name,
+		Phone:    request.Phone,
+		Password: request.Password,
+	}
+	_user.Create()
+
+	if _user.ID > 0 {
+		response.CreatedJSON(c, gin.H{
+			"data": _user,
+		})
+	} else {
+		response.Abort500(c, "创建用户失败，请稍后尝试~")
+	}
+}
---
+++ app/requests/validators/custom_rules.go
@@ -0,0 +1,62 @@
+// Package validators 存放自定义规则和验证器
+package validators
+
+import (
+	"errors"
+	"fmt"
+	"gohub/pkg/database"
+	"strings"
+
+	"github.com/thedevsaddam/govalidator"
+)
+
+// 此方法会在初始化时执行，注册自定义表单验证规则
+func init() {
+
+	// 自定义规则 not_exists，验证请求数据必须不存在于数据库中。
+	// 常用于保证数据库某个字段的值唯一，如用户名、邮箱、手机号、或者分类的名称。
+	// not_exists 参数可以有两种，一种是 2 个参数，一种是 3 个参数：
+	// not_exists:users,email 检查数据库表里是否存在同一条信息
+	// not_exists:users,email,32 排除用户掉 id 为 32 的用户
+	govalidator.AddCustomRule("not_exists", func(field string, rule string, message string, value interface{}) error {
+		rng := strings.Split(strings.TrimPrefix(rule, "not_exists:"), ",")
+
+		// 第一个参数，表名称，如 users
+		tableName := rng[0]
+		// 第二个参数，字段名称，如 email 或者 phone
+		dbFiled := rng[1]
+
+		// 第三个参数，排除 ID
+		var exceptID string
+		if len(rng) > 2 {
+			exceptID = rng[2]
+		}
+
+		// 用户请求过来的数据
+		requestValue := value.(string)
+
+		// 拼接 SQL
+		query := database.DB.Table(tableName).Where(dbFiled+" = ?", requestValue)
+
+		// 如果传参第三个参数，加上 SQL Where 过滤
+		if len(exceptID) > 0 {
+			query.Where("id != ?", exceptID)
+		}
+
+		// 查询数据库
+		var count int64
+		query.Count(&count)
+
+		// 验证不通过，数据库能找到对应的数据
+		if count != 0 {
+			// 如果有自定义错误消息的话
+			if message != "" {
+				return errors.New(message)
+			}
+			// 默认的错误消息
+			return fmt.Errorf("%v 已被占用", requestValue)
+		}
+		// 验证通过
+		return nil
+	})
+}
