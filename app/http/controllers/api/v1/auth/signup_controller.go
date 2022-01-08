cfed2bf2651e3831d364ef3cf99276fb3cc38cb9
Summer <summer@learnku.com>
2022-01-08 17:26:34 +0800

    JSON 解析和验证器封装

--- app/http/controllers/api/v1/auth/signup_controller.go
+++ app/http/controllers/api/v1/auth/signup_controller.go
@@ -2,7 +2,6 @@
 package auth
 
 import (
-	"fmt"
 	v1 "gohub/app/http/controllers/api/v1"
 	"gohub/app/models/user"
 	"gohub/app/requests"
@@ -18,30 +17,9 @@ type SignupController struct {
 
 // IsPhoneExist 检测手机号是否被注册
 func (sc *SignupController) IsPhoneExist(c *gin.Context) {
-
-	// 初始化请求对象
+	// 获取请求参数，并做表单验证
 	request := requests.SignupPhoneExistRequest{}
-
-	// 解析 JSON 请求
-	if err := c.ShouldBindJSON(&request); err != nil {
-		// 解析失败，返回 422 状态码和错误信息
-		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
-			"error": err.Error(),
-		})
-		// 打印错误信息
-		fmt.Println(err.Error())
-		// 出错了，中断请求
-		return
-	}
-
-	// 表单验证
-	errs := requests.ValidateSignupPhoneExist(&request, c)
-	// errs 返回长度等于零即通过，大于 0 即有错误发生
-	if len(errs) > 0 {
-		// 验证失败，返回 422 状态码和错误信息
-		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
-			"errors": errs,
-		})
+	if ok := requests.Validate(c, &request, requests.SignupPhoneExist); !ok {
 		return
 	}
 
@@ -53,34 +31,10 @@ func (sc *SignupController) IsPhoneExist(c *gin.Context) {
 
 // IsEmailExist 检测邮箱是否已注册
 func (sc *SignupController) IsEmailExist(c *gin.Context) {
-
-	// 初始化请求对象
 	request := requests.SignupEmailExistRequest{}
-
-	// 解析 JSON 请求
-	if err := c.ShouldBindJSON(&request); err != nil {
-		// 解析失败，返回 422 状态码和错误信息
-		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
-			"error": err.Error(),
-		})
-		// 打印错误信息
-		fmt.Println(err.Error())
-		// 出错了，中断请求
+	if ok := requests.Validate(c, &request, requests.SignupEmailExist); !ok {
 		return
 	}
-
-	// 表单验证
-	errs := requests.ValidateSignupEmailExist(&request, c)
-	// errs 返回长度等于零即通过，大于 0 即有错误发生
-	if len(errs) > 0 {
-		// 验证失败，返回 422 状态码和错误信息
-		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
-			"errors": errs,
-		})
-		return
-	}
-
-	//  检查数据库并返回响应
 	c.JSON(http.StatusOK, gin.H{
 		"exist": user.IsEmailExist(request.Email),
 	})
---
+++ app/requests/requests.go
@@ -0,0 +1,56 @@
+// Package requests 处理请求数据和表单验证
+package requests
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/thedevsaddam/govalidator"
+)
+
+// ValidatorFunc 验证函数类型
+type ValidatorFunc func(interface{}, *gin.Context) map[string][]string
+
+// Validate 控制器里调用示例：
+//        if ok := requests.Validate(c, &requests.UserSaveRequest{}, requests.UserSave); !ok {
+//            return
+//        }
+func Validate(c *gin.Context, obj interface{}, handler ValidatorFunc) bool {
+
+	// 1. 解析请求，支持 JSON 数据、表单请求和 URL Query
+	if err := c.ShouldBind(obj); err != nil {
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"message": "请求解析错误，请确认请求格式是否正确。上传文件请使用 multipart 标头，参数请使用 JSON 格式。",
+			"error":   err.Error(),
+		})
+		fmt.Println(err.Error())
+		return false
+	}
+
+	// 2. 表单验证
+	errs := handler(obj, c)
+
+	// 3. 判断验证是否通过
+	if len(errs) > 0 {
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"message": "请求验证不通过，具体请查看 errors",
+			"errors":  errs,
+		})
+		return false
+	}
+
+	return true
+}
+
+func validate(data interface{}, rules govalidator.MapData, messages govalidator.MapData) map[string][]string {
+	// 配置选项
+	opts := govalidator.Options{
+		Data:          data,
+		Rules:         rules,
+		TagIdentifier: "valid", // 模型中的 Struct 标签标识符
+		Messages:      messages,
+	}
+	// 开始验证
+	return govalidator.New(opts).ValidateStruct()
+}
