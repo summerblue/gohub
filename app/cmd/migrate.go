d9c16db062e7116dbdb9b4105aa4301acc63c88e
Summer <summer@learnku.com>
2022-01-11 21:42:54 +0800

    migrate rollback 命令

--- app/cmd/migrate.go
+++ app/cmd/migrate.go
@@ -22,6 +22,7 @@ var CmdMigrateUp = &cobra.Command{
 func init() {
 	CmdMigrate.AddCommand(
 		CmdMigrateUp,
+		CmdMigrateRollback,
 	)
 }
 
@@ -34,4 +35,16 @@ func migrator() *migrate.Migrator {
 
 func runUp(cmd *cobra.Command, args []string) {
 	migrator().Up()
+}
+
+var CmdMigrateRollback = &cobra.Command{
+	Use: "down",
+	// 设置别名 migrate down == migrate rollback
+	Aliases: []string{"rollback"},
+	Short:   "Reverse the up command",
+	Run:     runDown,
+}
+
+func runDown(cmd *cobra.Command, args []string) {
+	migrator().Rollback()
 }
