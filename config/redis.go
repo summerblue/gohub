f33fe80e0604f0d1b16d0c77459cbf8d2c3b9bc5
Summer <summer@learnku.com>
2022-01-12 21:50:12 +0800

    cache 包

--- config/redis.go
+++ config/redis.go
@@ -15,6 +15,9 @@ func init() {
 
 			// 业务类存储使用 1 (图片验证码、短信验证码、会话)
 			"database": config.Env("REDIS_MAIN_DB", 1),
+
+			// 缓存 cache 包使用 0 ，缓存清空理应当不影响业务
+			"database_cache": config.Env("REDIS_CACHE_DB", 0),
 		}
 	})
 }
