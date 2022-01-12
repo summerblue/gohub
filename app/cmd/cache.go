a09d7533d58de1c771d07a97aa9b8bba4e9c8a8b
Summer <summer@learnku.com>
2022-01-12 22:03:21 +0800

    cache clear 命令

---
+++ app/cmd/cache.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"gohub/pkg/cache"
+	"gohub/pkg/console"
+
+	"github.com/spf13/cobra"
+)
+
+var CmdCache = &cobra.Command{
+	Use:   "cache",
+	Short: "Cache management",
+}
+
+var CmdCacheClear = &cobra.Command{
+	Use:   "clear",
+	Short: "Clear cache",
+	Run:   runCacheClear,
+}
+
+func init() {
+	// 注册 cache 命令的子命令
+	CmdCache.AddCommand(CmdCacheClear)
+}
+
+func runCacheClear(cmd *cobra.Command, args []string) {
+	cache.Flush()
+	console.Success("Cache cleared.")
+}
