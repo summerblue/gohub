77d67b498728645127a8554892ee13d1d88df0d8
Summer <summer@learnku.com>
2022-01-12 19:03:01 +0800

    用户列表

--- app/models/user/user_util.go
+++ app/models/user/user_util.go
@@ -45,3 +45,9 @@ func GetByEmail(email string) (userModel User) {
 	database.DB.Where("email = ?", email).First(&userModel)
 	return
 }
+
+// All 获取所有用户数据
+func All() (users []User) {
+	database.DB.Find(&users)
+	return
+}
