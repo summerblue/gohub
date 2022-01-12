5506fd4176a544b24e466f903d862d521105e882
Summer <summer@learnku.com>
2022-01-12 22:16:55 +0800

    上传用户头像

--- routes/api.go
+++ routes/api.go
@@ -63,6 +63,7 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			usersGroup.PUT("/email", middlewares.AuthJWT(), uc.UpdateEmail)
 			usersGroup.PUT("/phone", middlewares.AuthJWT(), uc.UpdatePhone)
 			usersGroup.PUT("/password", middlewares.AuthJWT(), uc.UpdatePassword)
+			usersGroup.PUT("/avatar", middlewares.AuthJWT(), uc.UpdateAvatar)
 		}
 
 		cgc := new(controllers.CategoriesController)
