35c636ed6535061dba26ef94f878ea395bd4a592
Summer <summer@learnku.com>
2022-08-17 20:23:51 +0800

    初始化路由

---
+++ routes/api.go
@@ -0,0 +1,24 @@
+// Package routes 注册路由
+package routes
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// RegisterAPIRoutes 注册网页相关路由
+func RegisterAPIRoutes(r *gin.Engine) {
+
+	// 测试一个 v1 的路由组，我们所有的 v1 版本的路由都将存放到这里
+	v1 := r.Group("/v1")
+	{
+		// 注册一个路由
+		v1.GET("/", func(c *gin.Context) {
+			// 以 JSON 格式响应
+			c.JSON(http.StatusOK, gin.H{
+				"Hello": "World!",
+			})
+		})
+	}
+}
