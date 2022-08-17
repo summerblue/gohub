898f18cd79a3d44598bdb5952bd739f2aa29f4bc
Summer <summer@learnku.com>
2022-08-17 22:28:43 +0800

    HTTP 访问日志

---
+++ app/http/middlewares/logger.go
@@ -0,0 +1,78 @@
+// Package middlewares 存放系统中间件
+package middlewares
+
+import (
+	"bytes"
+	"gohub/pkg/helpers"
+	"gohub/pkg/logger"
+	"io/ioutil"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/cast"
+	"go.uber.org/zap"
+)
+
+type responseBodyWriter struct {
+	gin.ResponseWriter
+	body *bytes.Buffer
+}
+
+func (r responseBodyWriter) Write(b []byte) (int, error) {
+	r.body.Write(b)
+	return r.ResponseWriter.Write(b)
+}
+
+// Logger 记录请求日志
+func Logger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		// 获取 response 内容
+		w := &responseBodyWriter{body: &bytes.Buffer{}, ResponseWriter: c.Writer}
+		c.Writer = w
+
+		// 获取请求数据
+		var requestBody []byte
+		if c.Request.Body != nil {
+			// c.Request.Body 是一个 buffer 对象，只能读取一次
+			requestBody, _ = ioutil.ReadAll(c.Request.Body)
+			// 读取后，重新赋值 c.Request.Body ，以供后续的其他操作
+			c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(requestBody))
+		}
+
+		// 设置开始时间
+		start := time.Now()
+		c.Next()
+
+		// 开始记录日志的逻辑
+		cost := time.Since(start)
+		responStatus := c.Writer.Status()
+
+		logFields := []zap.Field{
+			zap.Int("status", responStatus),
+			zap.String("request", c.Request.Method+" "+c.Request.URL.String()),
+			zap.String("query", c.Request.URL.RawQuery),
+			zap.String("ip", c.ClientIP()),
+			zap.String("user-agent", c.Request.UserAgent()),
+			zap.String("errors", c.Errors.ByType(gin.ErrorTypePrivate).String()),
+			zap.String("time", helpers.MicrosecondsStr(cost)),
+		}
+		if c.Request.Method == "POST" || c.Request.Method == "PUT" || c.Request.Method == "DELETE" {
+			// 请求的内容
+			logFields = append(logFields, zap.String("Request Body", string(requestBody)))
+
+			// 响应的内容
+			logFields = append(logFields, zap.String("Response Body", w.body.String()))
+		}
+
+		if responStatus > 400 && responStatus <= 499 {
+			// 除了 StatusBadRequest 以外，warning 提示一下，常见的有 403 404，开发时都要注意
+			logger.Warn("HTTP Warning "+cast.ToString(responStatus), logFields...)
+		} else if responStatus >= 500 && responStatus <= 599 {
+			// 除了内部错误，记录 error
+			logger.Error("HTTP Error "+cast.ToString(responStatus), logFields...)
+		} else {
+			logger.Debug("HTTP Access Log", logFields...)
+		}
+	}
+}
--- main.go
+++ main.go
@@ -26,6 +26,12 @@ func main() {
 	// 初始化 Logger
 	bootstrap.SetupLogger()
 
+	// 设置 gin 的运行模式，支持 debug, release, test
+	// release 会屏蔽调试信息，官方建议生产环境中使用
+	// 非 release 模式 gin 终端打印太多信息，干扰到我们程序中的 Log
+	// 故此设置为 release，有特殊情况手动改为 debug 即可
+	gin.SetMode(gin.ReleaseMode)
+
 	// new 一个 Gin Engine 实例
 	router := gin.New()
 
