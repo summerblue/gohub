947221ff9c4bc32546ba58f7c9f0712b4d37913f
Summer <summer@learnku.com>
2022-08-17 20:42:21 +0800

    用户模型

---
+++ app/models/model.go
@@ -0,0 +1,17 @@
+// Package models 模型通用属性和方法
+package models
+
+import (
+	"time"
+)
+
+// BaseModel 模型基类
+type BaseModel struct {
+	ID uint64 `gorm:"column:id;primaryKey;autoIncrement;" json:"id,omitempty"`
+}
+
+// CommonTimestampsField 时间戳
+type CommonTimestampsField struct {
+	CreatedAt time.Time `gorm:"column:created_at;index;" json:"created_at,omitempty"`
+	UpdatedAt time.Time `gorm:"column:updated_at;index;" json:"updated_at,omitempty"`
+}
---
+++ app/models/user/user_util.go
@@ -0,0 +1,19 @@
+package user
+
+import (
+	"gohub/pkg/database"
+)
+
+// IsEmailExist 判断 Email 已被注册
+func IsEmailExist(email string) bool {
+	var count int64
+	database.DB.Model(User{}).Where("email = ?", email).Count(&count)
+	return count > 0
+}
+
+// IsPhoneExist 判断手机号已被注册
+func IsPhoneExist(phone string) bool {
+	var count int64
+	database.DB.Model(User{}).Where("phone = ?", phone).Count(&count)
+	return count > 0
+}
