d00b9ede9cb18927c28ccf1f5582d2d5e52a0909
Summer <summer@learnku.com>
2022-08-17 23:00:57 +0800

    图片验证码接口

---
+++ app/http/controllers/api/v1/auth/verify_code_controller.go
@@ -0,0 +1,28 @@
+package auth
+
+import (
+	v1 "gohub/app/http/controllers/api/v1"
+	"gohub/pkg/captcha"
+	"gohub/pkg/logger"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// VerifyCodeController 用户控制器
+type VerifyCodeController struct {
+	v1.BaseAPIController
+}
+
+// ShowCaptcha 显示图片验证码
+func (vc *VerifyCodeController) ShowCaptcha(c *gin.Context) {
+	// 生成验证码
+	id, b64s, err := captcha.NewCaptcha().GenerateCaptcha()
+	// 记录错误日志，因为验证码是用户的入口，出错时应该记 error 等级的日志
+	logger.LogIf(err)
+	// 返回给用户
+	c.JSON(http.StatusOK, gin.H{
+		"captcha_id":    id,
+		"captcha_image": b64s,
+	})
+}
--- routes/api.go
+++ routes/api.go
@@ -20,6 +20,11 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			authGroup.POST("/signup/phone/exist", suc.IsPhoneExist)
 			// 判断 Email 是否已注册
 			authGroup.POST("/signup/email/exist", suc.IsEmailExist)
+
+			// 发送验证码
+			vcc := new(auth.VerifyCodeController)
+			// 图片验证码，需要加限流
+			authGroup.POST("/verify-codes/captcha", vcc.ShowCaptcha)
 		}
 	}
 }
