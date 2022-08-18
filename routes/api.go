0343fb7f9666cd2d2e151e87a6b5568d42cb3bb4
Summer <summer@learnku.com>
2022-08-18 22:34:48 +0800

    API 域名和前缀

--- routes/api.go
+++ routes/api.go
@@ -5,6 +5,7 @@ import (
 	controllers "gohub/app/http/controllers/api/v1"
 	"gohub/app/http/controllers/api/v1/auth"
 	"gohub/app/http/middlewares"
+	"gohub/pkg/config"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,7 +14,12 @@ import (
 func RegisterAPIRoutes(r *gin.Engine) {
 
 	// 测试一个 v1 的路由组，我们所有的 v1 版本的路由都将存放到这里
-	v1 := r.Group("/v1")
+	var v1 *gin.RouterGroup
+	if len(config.Get("app.api_domain")) == 0 {
+		v1 = r.Group("/api/v1")
+	} else {
+		v1 = r.Group("/v1")
+	}
 
 	// 全局限流中间件：每小时限流。这里是所有 API （根据 IP）请求加起来。
 	// 作为参考 Github API 每小时最多 60 个请求（根据 IP）。
