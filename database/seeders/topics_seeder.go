193422e34fe7a269448f66ca5d010bc9f8151c41
Summer <summer@learnku.com>
2022-08-18 22:14:01 +0800

    话题列表

---
+++ database/seeders/topics_seeder.go
@@ -0,0 +1,28 @@
+package seeders
+
+import (
+    "fmt"
+    "gohub/database/factories"
+    "gohub/pkg/console"
+    "gohub/pkg/logger"
+    "gohub/pkg/seed"
+
+    "gorm.io/gorm"
+)
+
+func init() {
+
+    seed.Add("SeedTopicsTable", func(db *gorm.DB) {
+
+        topics  := factories.MakeTopics(10)
+
+        result := db.Table("topics").Create(&topics)
+
+        if err := result.Error; err != nil {
+            logger.LogIf(err)
+            return
+        }
+
+        console.Success(fmt.Sprintf("Table [%v] %v rows seeded", result.Statement.Table, result.RowsAffected))
+    })
+}
