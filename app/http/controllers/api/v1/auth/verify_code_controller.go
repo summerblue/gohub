9ee2ee22a622f60dca5510efba0c377e840595e7
Summer <summer@learnku.com>
2022-08-18 09:00:46 +0800

    发送短信验证码

--- app/http/controllers/api/v1/auth/verify_code_controller.go
+++ app/http/controllers/api/v1/auth/verify_code_controller.go
@@ -2,9 +2,11 @@ package auth
 
 import (
 	v1 "gohub/app/http/controllers/api/v1"
+	"gohub/app/requests"
 	"gohub/pkg/captcha"
 	"gohub/pkg/logger"
 	"gohub/pkg/response"
+	"gohub/pkg/verifycode"
 
 	"github.com/gin-gonic/gin"
 )
@@ -25,4 +27,21 @@ func (vc *VerifyCodeController) ShowCaptcha(c *gin.Context) {
 		"captcha_id":    id,
 		"captcha_image": b64s,
 	})
+}
+
+// SendUsingPhone 发送手机验证码
+func (vc *VerifyCodeController) SendUsingPhone(c *gin.Context) {
+
+	// 1. 验证表单
+	request := requests.VerifyCodePhoneRequest{}
+	if ok := requests.Validate(c, &request, requests.VerifyCodePhone); !ok {
+		return
+	}
+
+	// 2. 发送 SMS
+	if ok := verifycode.NewVerifyCode().SendSMS(request.Phone); !ok {
+		response.Abort500(c, "发送短信失败~")
+	} else {
+		response.Success(c)
+	}
 }
