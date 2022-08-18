b274bab54ae4da5d186ff6a6e2f39465963b76ca
Summer <summer@learnku.com>
2022-08-18 11:17:54 +0800

    make migration 命令

--- app/cmd/make/make.go
+++ app/cmd/make/make.go
@@ -75,6 +75,7 @@ func init() {
 		CmdMakeModel,
 		CmdMakeAPIController,
 		CmdMakeRequest,
+		CmdMakeMigration,
 	)
 }
 
---
+++ database/migrations/2022_08_18_111518_add_users_table.go
@@ -0,0 +1,33 @@
+package migrations
+
+import (
+	"database/sql"
+	"gohub/app/models"
+	"gohub/pkg/migrate"
+
+	"gorm.io/gorm"
+)
+
+func init() {
+
+	type User struct {
+		models.BaseModel
+
+		Name     string `gorm:"type:varchar(255);not null;index"`
+		Email    string `gorm:"type:varchar(255);index;default:null"`
+		Phone    string `gorm:"type:varchar(20);index;default:null"`
+		Password string `gorm:"type:varchar(255)"`
+
+		models.CommonTimestampsField
+	}
+
+	up := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.AutoMigrate(&User{})
+	}
+
+	down := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.DropTable(&User{})
+	}
+
+	migrate.Add("2022_08_18_111518_add_users_table", up, down)
+}
