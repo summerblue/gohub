f0a6cc27730f2ce91874bfdb88445c4a52cf4bb0
Summer <summer@learnku.com>
2022-08-18 09:54:36 +0800

    使用密码登录

--- app/http/controllers/api/v1/auth/login_controller.go
+++ app/http/controllers/api/v1/auth/login_controller.go
@@ -38,3 +38,25 @@ func (lc *LoginController) LoginByPhone(c *gin.Context) {
 		})
 	}
 }
+
+// LoginByPassword 多种方法登录，支持手机号、email 和用户名
+func (lc *LoginController) LoginByPassword(c *gin.Context) {
+	// 1. 验证表单
+	request := requests.LoginByPasswordRequest{}
+	if ok := requests.Validate(c, &request, requests.LoginByPassword); !ok {
+		return
+	}
+
+	// 2. 尝试登录
+	user, err := auth.Attempt(request.LoginID, request.Password)
+	if err != nil {
+		// 失败，显示错误提示
+		response.Unauthorized(c, "账号不存在或密码错误")
+
+	} else {
+		token := jwt.NewJWT().IssueToken(user.GetStringID(), user.Name)
+		response.JSON(c, gin.H{
+			"token": token,
+		})
+	}
+}
