3b2bcba05c791a489481a1b0ae3b7cd23b431282
Summer <summer@learnku.com>
2022-08-18 21:02:04 +0800

    分类模型

---
+++ database/migrations/2022_08_18_210112_add_categories_table.go
@@ -0,0 +1,31 @@
+package migrations
+
+import (
+	"database/sql"
+	"gohub/app/models"
+	"gohub/pkg/migrate"
+
+	"gorm.io/gorm"
+)
+
+func init() {
+
+	type Category struct {
+		models.BaseModel
+
+		Name        string `gorm:"type:varchar(255);not null;index"`
+		Description string `gorm:"type:varchar(255);default:null"`
+
+		models.CommonTimestampsField
+	}
+
+	up := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.AutoMigrate(&Category{})
+	}
+
+	down := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.DropTable(&Category{})
+	}
+
+	migrate.Add("2022_08_18_210112_add_categories_table", up, down)
+}
