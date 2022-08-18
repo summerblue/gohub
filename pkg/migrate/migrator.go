bd7a983c981936af9eb2131eaa4f9a71c96ec64a
Summer <summer@learnku.com>
2022-08-18 11:20:45 +0800

    migrate reset 和 fresh 命令

--- pkg/migrate/migrator.go
+++ pkg/migrate/migrator.go
@@ -185,3 +185,27 @@ func (migrator *Migrator) runUpMigration(mfile MigrationFile, batch int) {
 	err := migrator.DB.Create(&Migration{Migration: mfile.FileName, Batch: batch}).Error
 	console.ExitIf(err)
 }
+
+// Reset 回滚所有迁移
+func (migrator *Migrator) Reset() {
+
+	migrations := []Migration{}
+
+	// 按照倒序读取所有迁移文件
+	migrator.DB.Order("id DESC").Find(&migrations)
+
+	// 回滚所有迁移
+	if !migrator.rollbackMigrations(migrations) {
+		console.Success("[migrations] table is empty, nothing to reset.")
+	}
+}
+
+// Refresh 回滚所有迁移，并运行所有迁移
+func (migrator *Migrator) Refresh() {
+
+	// 回滚所有迁移
+	migrator.Reset()
+
+	// 再次执行所有迁移
+	migrator.Up()
+}
