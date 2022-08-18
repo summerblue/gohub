dadafaa400084897c3c78799623303828e25b4ac
Summer <summer@learnku.com>
2022-08-18 11:21:46 +0800

    migrate fresh 命令

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -3,7 +3,9 @@ package database
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
+	"gohub/pkg/config"
 
 	"gorm.io/gorm"
 	gormlogger "gorm.io/gorm/logger"
@@ -31,4 +33,70 @@ func Connect(dbConfig gorm.Dialector, _logger gormlogger.Interface) {
 	if err != nil {
 		fmt.Println(err.Error())
 	}
+}
+func CurrentDatabase() (dbname string) {
+	dbname = DB.Migrator().CurrentDatabase()
+	return
+}
+
+func DeleteAllTables() error {
+	var err error
+	switch config.Get("database.connection") {
+	case "mysql":
+		err = deleteMySQLTables()
+	case "sqlite":
+		deleteAllSqliteTables()
+	default:
+		panic(errors.New("database connection not supported"))
+	}
+
+	return err
+}
+
+func deleteAllSqliteTables() error {
+	tables := []string{}
+
+	// 读取所有数据表
+	err := DB.Select(&tables, "SELECT name FROM sqlite_master WHERE type='table'").Error
+	if err != nil {
+		return err
+	}
+
+	// 删除所有表
+	for _, table := range tables {
+		err := DB.Migrator().DropTable(table)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func deleteMySQLTables() error {
+	dbname := CurrentDatabase()
+	tables := []string{}
+
+	// 读取所有数据表
+	err := DB.Table("information_schema.tables").
+		Where("table_schema = ?", dbname).
+		Pluck("table_name", &tables).
+		Error
+	if err != nil {
+		return err
+	}
+
+	// 暂时关闭外键检测
+	DB.Exec("SET foreign_key_checks = 0;")
+
+	// 删除所有表
+	for _, table := range tables {
+		err := DB.Migrator().DropTable(table)
+		if err != nil {
+			return err
+		}
+	}
+
+	// 开启 MySQL 外键检测
+	DB.Exec("SET foreign_key_checks = 1;")
+	return nil
 }
