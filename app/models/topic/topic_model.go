27a8ffa3682b1986b1b4640bd74c8c61c062ad0c
Summer <summer@learnku.com>
2022-08-18 21:46:07 +0800

    话题模型和迁移

---
+++ app/models/topic/topic_model.go
@@ -0,0 +1,40 @@
+// Package topic 模型
+package topic
+
+import (
+	"gohub/app/models"
+	"gohub/app/models/category"
+	"gohub/app/models/user"
+	"gohub/pkg/database"
+)
+
+type Topic struct {
+	models.BaseModel
+
+	Title      string `json:"title,omitempty" `
+	Body       string `json:"body,omitempty" `
+	UserID     string `json:"user_id,omitempty"`
+	CategoryID string `json:"category_id,omitempty"`
+
+	// 通过 user_id 关联用户
+	User user.User `json:"user"`
+
+	// 通过 category_id 关联分类
+	Category category.Category `json:"category"`
+
+	models.CommonTimestampsField
+}
+
+func (topic *Topic) Create() {
+	database.DB.Create(&topic)
+}
+
+func (topic *Topic) Save() (rowsAffected int64) {
+	result := database.DB.Save(&topic)
+	return result.RowsAffected
+}
+
+func (topic *Topic) Delete() (rowsAffected int64) {
+	result := database.DB.Delete(&topic)
+	return result.RowsAffected
+}
---
+++ database/migrations/2022_08_18_214515_add_topics_table.go
@@ -0,0 +1,44 @@
+package migrations
+
+import (
+	"database/sql"
+	"gohub/app/models"
+	"gohub/pkg/migrate"
+
+	"gorm.io/gorm"
+)
+
+func init() {
+
+	type User struct {
+		models.BaseModel
+	}
+	type Category struct {
+		models.BaseModel
+	}
+
+	type Topic struct {
+		models.BaseModel
+
+		Title      string `gorm:"type:varchar(255);not null;index"`
+		Body       string `gorm:"type:longtext;not null"`
+		UserID     string `gorm:"type:bigint;not null;index"`
+		CategoryID string `gorm:"type:bigint;not null;index"`
+
+		// 会创建 user_id 和 category_id 外键的约束
+		User     User
+		Category Category
+
+		models.CommonTimestampsField
+	}
+
+	up := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.AutoMigrate(&Topic{})
+	}
+
+	down := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.DropTable(&Topic{})
+	}
+
+	migrate.Add("2022_08_18_214515_add_topics_table", up, down)
+}
