7b069661a527a5283445a489b48f7dd997a1a6e1
Summer <summer@learnku.com>
2022-08-18 21:05:53 +0800

    更新分类

--- app/http/controllers/api/v1/categories_controller.go
+++ app/http/controllers/api/v1/categories_controller.go
@@ -30,3 +30,30 @@ func (ctrl *CategoriesController) Store(c *gin.Context) {
 		response.Abort500(c, "创建失败，请稍后尝试~")
 	}
 }
+
+func (ctrl *CategoriesController) Update(c *gin.Context) {
+
+	// 验证 url 参数 id 是否正确
+	categoryModel := category.Get(c.Param("id"))
+	if categoryModel.ID == 0 {
+		response.Abort404(c)
+		return
+	}
+
+	// 表单验证
+	request := requests.CategoryRequest{}
+	if ok := requests.Validate(c, &request, requests.CategorySave); !ok {
+		return
+	}
+
+	// 保存数据
+	categoryModel.Name = request.Name
+	categoryModel.Description = request.Description
+	rowsAffected := categoryModel.Save()
+
+	if rowsAffected > 0 {
+		response.Data(c, categoryModel)
+	} else {
+		response.Abort500(c)
+	}
+}
--- routes/api.go
+++ routes/api.go
@@ -63,7 +63,7 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			cgc := new(controllers.CategoriesController)
 			cgcGroup := v1.Group("/categories")
 			{
-				cgcGroup.POST("", middlewares.AuthJWT(), cgc.Store)
+				cgcGroup.PUT("/:id", middlewares.AuthJWT(), cgc.Update)
 			}
 		}
 	}
