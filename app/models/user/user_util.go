54dcf23bbd6e74ef97b93d71804bf2f7dfd8f05a
Summer <summer@learnku.com>
2022-08-18 10:36:37 +0800

    Auth 中间件

--- app/models/user/user_util.go
+++ app/models/user/user_util.go
@@ -33,3 +33,9 @@ func GetByMulti(loginID string) (userModel User) {
 		First(&userModel)
 	return
 }
+
+// Get 通过 ID 获取用户
+func Get(idstr string) (userModel User) {
+	database.DB.Where("id", idstr).First(&userModel)
+	return
+}
