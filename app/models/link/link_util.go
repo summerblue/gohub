fa5171cb49f9d0f6d22937d6de8b0648dc3c4601
Summer <summer@learnku.com>
2022-08-18 22:16:27 +0800

    友情链接模型和迁移

---
+++ app/models/link/link_util.go
@@ -0,0 +1,37 @@
+package link
+
+import (
+    "gohub/pkg/database"
+)
+
+func Get(idstr string) (link Link) {
+    database.DB.Where("id", idstr).First(&link)
+    return
+}
+
+func GetBy(field, value string) (link Link) {
+    database.DB.Where("? = ?", field, value).First(&link)
+    return
+}
+
+func All() (links []Link) {
+    database.DB.Find(&links)
+    return 
+}
+
+func IsExist(field, value string) bool {
+    var count int64
+    database.DB.Model(Link{}).Where("? = ?", field, value).Count(&count)
+    return count > 0
+}
+
+func Paginate(c *gin.Context, perPage int) (links []Link, paging paginator.Paging) {
+    paging = paginator.Paginate(
+        c,
+        database.DB.Model(Link{}),
+        &links,
+        app.V1URL(database.TableName(&Link{})),
+        perPage,
+    )
+    return
+}
